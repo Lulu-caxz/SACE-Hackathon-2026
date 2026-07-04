f7ec2b60ca1278bbcbd31fd3d02fcbcab486cab4
Luiz tavares <tavaresluiz216@gmail.com>
2026-07-04 20:02:30 -0300

    rotas_corrijidas

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,19 @@
-import Home from './pages/Home/index'; // Ajuste o caminho se estiver dentro de uma pasta (ex: './components/selecao-escola')
+import { Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home/index';
+import NotFound from "./pages/NotFound/index";
+
+import SelecaoEscola from "./pages/escola/selecao-escola"; 
+
+import MainLayout from "./layouts/MainLayout";
 
 export default function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/escolas" element={<SelecaoEscola />} />
+            <Route element={<MainLayout />}></Route>
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 }
